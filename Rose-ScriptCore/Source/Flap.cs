3198e20e0e6924762c07d48e6d98d69e8c662331
Sam <samcashlamore@gmail.com>
2022-08-03 10:34:56 -0400

    C# Lerp

--- Rose-ScriptCore/Source/Flap.cs
+++ Rose-ScriptCore/Source/Flap.cs
@@ -9,7 +9,8 @@ namespace Sandbox
         private Rigidbody2DComponent m_RigidBody2D;
         private TransformComponent m_Transform;
 
-        bool m_SpaceWasDown;
+        private float time = 0.0f;
+
         void OnCreate()
         {
             Console.WriteLine($"Player.OnCreate - {ID}");
@@ -22,17 +23,25 @@ namespace Sandbox
 
             if (Input.IsKeyDown(KeyCode.Space))
             {
-                m_Transform.Scale = new Vector3(1.1f);
-
-                m_RigidBody2D.ApplyLinearImpulse(new Vector2(0.0f, 20.0f*ts), true);
-
-                m_SpaceWasDown = true;
+                time += ts * 5;
+                if(time > 1.0f)
+                {
+                    time = 1.0f;
+                }
+                
+                m_RigidBody2D.ApplyLinearImpulse(new Vector2(0.0f, 19.0f*ts), true);
             } else
             {
-                m_Transform.Scale = new Vector3(1.0f);
-                m_SpaceWasDown = false;
+                time -= ts * 5;
+                if (time < 0.0f)
+                {
+                    time = 0.0f;
+                }
             }
-                
+
+            float scale = Rose.Math.Lerp(1.0f, 1.1f, time);
+            //Console.WriteLine(scale);
+            m_Transform.Scale = new Vector3(scale);
         }
     }
 }
---
+++ Rose-ScriptCore/Source/Rose/Math.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Rose
+{
+    //TODO Consider changing the name of this calls to make it less ambiguous between system and rose math
+    public static class Math
+    {
+        static public float Lerp(float min, float max, float time)
+        {
+            return (1 - time) * min + time * max;
+        }
+    }
+}
