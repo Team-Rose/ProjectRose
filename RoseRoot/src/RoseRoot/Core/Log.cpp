ab8ef08585a27913d3e2a5907d362f1b25127685
Sam <samcashlamore@gmail.com>
2022-09-17 16:36:13 -0400

    Some fixes to the logger

--- RoseRoot/src/RoseRoot/Core/Log.cpp
+++ RoseRoot/src/RoseRoot/Core/Log.cpp
@@ -2,6 +2,8 @@
 
 #include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/sinks/basic_file_sink.h"
+
+#define RR_HAS_CONSOLE !RR_DIST
 namespace Rose
 {
 	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
@@ -11,16 +13,35 @@ namespace Rose
 	{
 		spdlog::set_pattern("%^[%T] %n: %v%$");
 
-		auto  sharedFileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("log.log");
+		std::vector<spdlog::sink_ptr> roseSinks =
+		{
+			std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/ROSE.log", true),
+#if RR_HAS_CONSOLE
+			std::make_shared<spdlog::sinks::stdout_color_sink_mt>()
+#endif
+		};
 
-		std::vector<spdlog::sink_ptr> sinks;
-		sinks.push_back(std::make_shared<spdlog::sinks::stdout_color_sink_st>());
-		sinks.push_back(sharedFileSink);
+		std::vector<spdlog::sink_ptr> appSinks =
+		{
+			std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/APP.log", true),
+#if RR_HAS_CONSOLE
+			std::make_shared<spdlog::sinks::stdout_color_sink_mt>()
+#endif
+		};
 
-		s_CoreLogger = std::make_shared<spdlog::logger>("ROSE", begin(sinks), end(sinks));
+
+		roseSinks[0]->set_pattern("[%T] [%l] %n: %v");
+		appSinks[0]->set_pattern("[%T] [%l] %n: %v");
+
+#if RR_HAS_CONSOLE
+		roseSinks[1]->set_pattern("%^[%T] %n: %v%$");
+		appSinks[1]->set_pattern("%^[%T] %n: %v%$");
+#endif
+
+		s_CoreLogger = std::make_shared<spdlog::logger>("ROSE", begin(roseSinks), end(roseSinks));
 		s_CoreLogger->set_level(spdlog::level::trace);
 		
-		s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(sinks), end(sinks));
+		s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(appSinks), end(appSinks));
 		s_ClientLogger->set_level(spdlog::level::trace);
 	}
 }
